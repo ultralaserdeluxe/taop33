6c33874ab94ed4e7b81a851bf62dcf3e2d404974
Alexander Yngve <aleyn573@student.liu.se>
2015-01-20 17:41:17 +0100

    Added solutions.

---
+++ lab_4/floc2.sol
@@ -0,0 +1,30 @@
+ *** Optimum ***
+Kostnad v=15252
+Flode 1,4 = 66 
+Flode 1,7 = 55 
+Flode 1,11 = 98 
+Flode 3,2 = 66 
+Flode 3,6 = 93 
+Flode 3,14 = 62 
+Flode 4,1 = 66 
+Flode 4,9 = 98 
+Flode 4,17 = 65 
+Flode 5,5 = 78 
+Flode 5,10 = 86 
+Flode 5,12 = 60 
+Flode 6,1 = 3 
+Flode 6,7 = 18 
+Flode 6,15 = 71 
+Flode 10,3 = 67 
+Flode 10,12 = 5 
+Flode 10,13 = 99 
+Flode 10,16 = 99 
+Flode 11,8 = 75 
+Flode 11,18 = 94 
+Fabrik 1 byggs 
+Fabrik 3 byggs 
+Fabrik 4 byggs 
+Fabrik 5 byggs 
+Fabrik 6 byggs 
+Fabrik 10 byggs 
+Fabrik 11 byggs 
---
+++ lab_4/floc3.sol
@@ -0,0 +1,30 @@
+ *** Optimum ***
+Kostnad v=244101
+Flode 2,3 = 23 
+Flode 2,5 = 18 
+Flode 2,10 = 31 
+Flode 2,16 = 39 
+Flode 2,21 = 38 
+Flode 2,25 = 24 
+Flode 9,1 = 49 
+Flode 9,2 = 29 
+Flode 9,6 = 17 
+Flode 9,7 = 48 
+Flode 9,9 = 15 
+Flode 9,12 = 31 
+Flode 9,13 = 14 
+Flode 9,14 = 44 
+Flode 9,15 = 23 
+Flode 9,18 = 45 
+Flode 9,22 = 26 
+Flode 10,4 = 40 
+Flode 10,8 = 13 
+Flode 10,11 = 16 
+Flode 10,17 = 33 
+Flode 10,19 = 40 
+Flode 10,20 = 43 
+Flode 10,23 = 47 
+Flode 10,24 = 34 
+Fabrik 2 byggs 
+Fabrik 9 byggs 
+Fabrik 10 byggs 
