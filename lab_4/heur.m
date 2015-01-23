671c0abdabbc36324ab14712440359bf362ca341
Alexander Yngve <aleyn573@student.liu.se>
2015-01-23 15:43:55 +0100

    Changed encoding.

--- lab_4/heur.m
+++ lab_4/heur.m
@@ -1,26 +1,26 @@
-% läs in indatafil
+% lÃ¤s in indatafil
 load(input('Ange datafil: ','s'));
 
 tic % ta tid
 
-x = zeros(size(c));  % x-matris med flöden mellan fabrik och kund.
-y = zeros(size(f));  % y-vektor med ettor för fabriker som ska byggas
+x = zeros(size(c));  % x-matris med flÃ¶den mellan fabrik och kund.
+y = zeros(size(f));  % y-vektor med ettor fÃ¶r fabriker som ska byggas
 
 disp(sprintf('Antal fabriker: %d, antal kunder: %d',m,n));
-disp(sprintf('Total kapacitet: %d, total efterfrågan: %d',sum(s),sum(d)));
+disp(sprintf('Total kapacitet: %d, total efterfrÃ¥gan: %d',sum(s),sum(d)));
 
-ss=s;      % återstående fabrikskapacitet, minska då transporter sker
-dd=d;      % återstående efterfrågan, minska då transporter sker
-cc=c;      % temp c, kan ändras
-ff=f;      % temp f, kan ändras
-           % ss,dd,cc,ff kan ändras, s,d,f,c bör ej ändras
+ss=s;      % Ã¥terstÃ¥ende fabrikskapacitet, minska dÃ¥ transporter sker
+dd=d;      % Ã¥terstÃ¥ende efterfrÃ¥gan, minska dÃ¥ transporter sker
+cc=c;      % temp c, kan Ã¤ndras
+ff=f;      % temp f, kan Ã¤ndras
+           % ss,dd,cc,ff kan Ã¤ndras, s,d,f,c bÃ¶r ej Ã¤ndras
 
-% sätt värdet på e
+% sÃ¤tt vÃ¤rdet pÃ¥ e
 e=1;
 
-while sum(dd)>0    %loopa till alla kunders efterfrågan är uppfylld
+while sum(dd)>0    %loopa till alla kunders efterfrÃ¥gan Ã¤r uppfylld
 
-    % välj fabrik, välj kund, skicka, på billigaste sätt
+    % vÃ¤lj fabrik, vÃ¤lj kund, skicka, pÃ¥ billigaste sÃ¤tt
     % uppdatera x, y, ss och dd, samt ev ff och cc
 
 end
