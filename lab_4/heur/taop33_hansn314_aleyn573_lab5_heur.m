d8a7bdb51921733a9d9258ed8c60d6c6a4f91aab
hannessnogren <hansn314@student.liu.se>
2015-01-29 15:21:14 +0100

    Updated header lab 5 heur

--- lab_4/heur/taop33_hansn314_aleyn573_lab5_heur.m
+++ lab_4/heur/taop33_hansn314_aleyn573_lab5_heur.m
@@ -1,3 +1,7 @@
+% TAOP33 - Laboration 5
+% Av: Hannes Snögren (hansn314) & Alexander Yngve (aleyn573)
+% En egen, enkel heurestik baserad på kodskelettet från kurshemsidan
+
 % läs in indatafil
 load(input('Ange datafil: ','s'));
 
@@ -10,7 +14,7 @@ disp(sprintf('Antal fabriker: %d, antal kunder: %d',m,n));
 disp(sprintf('Total kapacitet: %d, total efterfrågan: %d',sum(s),sum(d)));
 
 % Sätt e
-e = 100;
+e = 1;
 
 % initiera totalkostnad
 totalkostnad = 0;
@@ -26,47 +30,47 @@ while sum(dd)>0    %loopa till alla kunders efterfrågan är uppfylld
 
     % Hitta minsta element
     [row, col] = find(cc == min(cc(:)));
-    
+
     row = row(1);
     col = col(1);
-    
+
     % Hitta begränsning
     maxflow = min(ss(row), dd(col));
-    
+
     % Giltigt flöde?
     if maxflow > 0
         % Ta bort fabrikskostnad från bågkostnad
         cc(row,:) = cc(row,:) - ff(row);
-        
+
         % Lägg flödeskostnad i totalkostnad
         totalkostnad = totalkostnad + maxflow * c(row,col);
         x(row,col) = maxflow;
-        
+
         % Dra bort flöde från fabrikskapacitet och kundefterfrågan
         ss(row) = ss(row) - maxflow;
         dd(col) = dd(col) - maxflow;
-        
+
         % Lägg till fabrikskostnad på totalkostnad
         totalkostnad = totalkostnad + ff(row);
-        
+
         % Sätt frabrikskostnad till 0 och sätt fabrik i y
         ff(row) = 0;
         y(row) = 1;
-        
+
         % Sätt bågkostnad till inf, för att inte loopa
         cc(row,col) = inf;
     else
         % Om fabrikskapacitet eller kundefterfrågan är 0, sätt rad/kolumn
         % till inf, för att spara iterationer.
-        
+
         if ss(row) == 0
             cc(row,:) = inf;
         end
-        
+
         if dd(col) == 0
             cc(:,col) = inf;
         end
-        
+
     end
 
 end
